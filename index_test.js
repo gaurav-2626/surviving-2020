c02ca65531dc3cd00c54a48d2d9eb2d331a8722f
Tarun Dhankhar <tdhankhar@gmail.com>
2021-01-09 16:24:21 +0530

    dashboard upload

--- index_test.js
+++ index_test.js
@@ -1,5 +1,60 @@
-const num_rows = 8;
+const num_rows = 9;
 const num_cols = 12;
+
+let players = [
+  {
+    id: 1,
+    turn: true,
+    finished: false,
+    position: 1,
+    money: 1000,
+  },
+  {
+    id: 2,
+    turn: false,
+    finished: false,
+    position: 1,
+    money: 1000,
+  },
+  {
+    id: 3,
+    turn: false,
+    finished: false,
+    position: 1,
+    money: 1000,
+  },
+  {
+    id: 4,
+    turn: false,
+    finished: false,
+    position: 1,
+    money: 1000,
+  },
+];
+
+let turn = 0;
+
+const showTurn = (turn) => {
+  let playersCard = document.querySelectorAll(".player");
+
+  playersCard.forEach((playerCard) => {
+    let index = Number.parseInt(playerCard.id[6]);
+    if (index === turn) {
+      playerCard.style.border = "red 5px solid";
+    } else {
+      playerCard.style.border = "none";
+    }
+  });
+};
+
+const rollDice = () => {
+  let value = Math.floor(Math.random() * 6) + 1;
+  let span = document.querySelector("#dice-value");
+  span.textContent = value;
+
+  turn = (turn + 1) % 4;
+  showTurn(turn);
+};
 
 const makecells = () => {
   let board = document.querySelector(".board-rows");
@@ -9,8 +64,13 @@ const makecells = () => {
     ri.setAttribute("id", `row${r}`);
     for (let c = 1; c <= num_cols; c += 1) {
       let ci = document.createElement("div");
+      let cellId = 12 * (r - 1) + c;
+      if (r % 2 == 0) {
+        cellId = 12 * r - c + 1;
+      }
+      ci.textContent = cellId;
       ci.className = "col-1 cell";
-      ci.setAttribute("id", `cell${c}`);
+      ci.setAttribute("id", `cell${cellId}`);
       ri.appendChild(ci);
     }
     board.appendChild(ri);
@@ -18,3 +78,4 @@ const makecells = () => {
 };
 
 makecells();
+showTurn(0);
